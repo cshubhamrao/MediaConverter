f757073c09ffb870724d87d4b378e1d0ef413dba
Shubham Rao <cshubhamrao@gmail.com>
2014-03-26 18:00:26 +0530

    Add Options Dialog box
    And rename AboutForm.java to AboutDialog.java

--- Media Converter/src/com/github/cshubhamrao/MediaConverter/AboutDialog.java
+++ Media Converter/src/com/github/cshubhamrao/MediaConverter/AboutDialog.java
@@ -13,6 +13,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *//*
+ * Copyright (C) 2014 Shubham Rao
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.github.cshubhamrao.MediaConverter;
@@ -21,12 +36,12 @@ package com.github.cshubhamrao.MediaConverter;
  *
  * @author Shubham Rao
  */
-public class AboutForm extends javax.swing.JPanel {
+public class AboutDialog extends javax.swing.JPanel {
 
     /**
      * Creates new form AboutForm
      */
-    public AboutForm() {
+    public AboutDialog() {
         initComponents();
     }
 

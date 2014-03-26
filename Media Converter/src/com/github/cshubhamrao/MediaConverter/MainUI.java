6251aaf70f3d35a3b8fdb181bd22289473a4e71e
Shubham Rao <cshubhamrao@gmail.com>
2014-03-26 18:34:17 +0530

    Set System LAF
    use a Lambda Expression for invoking the form. Remove license header
    which appears twice.

--- Media Converter/src/com/github/cshubhamrao/MediaConverter/MainUI.java
+++ Media Converter/src/com/github/cshubhamrao/MediaConverter/MainUI.java
@@ -13,33 +13,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *//*
- * Copyright (C) 2014 Shubham Rao
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.github.cshubhamrao.MediaConverter;
 
-/**
+import java.util.logging.Level;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/** This class is the main UI for the app.
  *
  * @author Shubham Rao
  */
 public class MainUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form MainUI
      */
     public MainUI() {
         initComponents();
@@ -180,37 +168,22 @@ public class MainUI extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_exitButtonActionPerformed
 
-    /**
+    /** This is the main method for {@code MainUI}
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            /* Set the System look and feel */
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager
+                    .getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | 
+                IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainUI.class.getName()).log(java.
+                    util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
+        
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new MainUI().setVisible(true);
-        });
+        java.awt.EventQueue.invokeLater(() -> {new MainUI().setVisible(true);});
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables

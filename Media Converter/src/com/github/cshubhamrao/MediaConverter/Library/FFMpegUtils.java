aceff1adc03c77390739014600c1a6820c811aa8
Shubham Rao <cshubhamrao@gmail.com>
2014-03-26 16:43:27 +0530

    Add FFMpegUtils.java

---
+++ Media Converter/src/com/github/cshubhamrao/MediaConverter/Library/FFMpegUtils.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Shubham Rao
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.cshubhamrao.MediaConverter.Library;
+
+/** This class contains various methods and fields dealing with FFMpeg and its 
+ * executable
+ * 
+ * @version 1.0.0
+ * @author Shubham Rao
+ */
+public class FFMpegUtils {
+    
+}

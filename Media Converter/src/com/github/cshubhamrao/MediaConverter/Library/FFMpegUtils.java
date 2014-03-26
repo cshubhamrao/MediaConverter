0ee77b14d0595ed5d2161ab3359dbe1689832618
Shubham Rao <cshubhamrao@gmail.com>
2014-03-26 17:41:59 +0530

    Add methods to FFMpegUtils to set executable

--- Media Converter/src/com/github/cshubhamrao/MediaConverter/Library/FFMpegUtils.java
+++ Media Converter/src/com/github/cshubhamrao/MediaConverter/Library/FFMpegUtils.java
@@ -22,7 +22,7 @@ import java.io.File;
 /** This class contains various methods and fields dealing with FFMpeg and its 
  * executable
  * 
- * @version 1.0.0
+ * @version 1.0.1
  * @author Shubham Rao
  */
 public class FFMpegUtils {
@@ -36,4 +36,38 @@ public class FFMpegUtils {
      */
     public static File FFMPEG_EXECUTABLE = new File (OSUtils.getJarLocation()
                                                         .getParent(),"FFMpeg/");
+    
+    /** This method sets {@code FFMPEG_EXECUTABLE} based on the value of {@code
+     * currentOS} 
+     * 
+     * @since 1.0.1
+     * @param OS The OS for which to set the executable.
+     * @return The {@code File} object for {@code FFMPEG_EXECUTABLE}
+     */
+    public static File setFFMpegExecutable (OSUtils.OperatingSystem OS) {
+        File ffmpegExecutable = FFMpegUtils.FFMPEG_EXECUTABLE;
+        switch (OS) {
+            case LINUX:
+                ffmpegExecutable = new File (FFMPEG_EXECUTABLE,"/ffmpeg.bin");
+                break;
+            case MAC:
+                ffmpegExecutable = new File (FFMPEG_EXECUTABLE,"/ffmpeg");
+                break;
+            case WINDOWS:
+                ffmpegExecutable = new File (FFMPEG_EXECUTABLE,"/ffmpeg.exe");
+                break;
+        }
+        return ffmpegExecutable;
+    }
+    
+    /** This method sets {@code FFMPEG_EXECUTABLE} based on the current
+     * Operating System.
+     *
+     * @return The {@code File} object for {@code FFMPEG_EXECUTABLE}
+     */
+    public static File setFFMpegExecutable() {
+        FFMPEG_EXECUTABLE = FFMpegUtils.setFFMpegExecutable(OSUtils
+                                                               .getCurrentOS());
+        return FFMpegUtils.FFMPEG_EXECUTABLE;
+    }
 }

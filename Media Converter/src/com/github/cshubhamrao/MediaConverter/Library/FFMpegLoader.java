43b0ef4a95a19b01d9e11d76d0bdaf0673e26cf5
Shubham Rao <cshubhamrao@gmail.com>
2014-09-29 19:06:32 +0530

    Add code to extract ffmpeg executable :smile:
    No messy hardcoding of ffmpeg location. Now, ffmpeg executable will be
    stored in app's .jar and then extracted temporarily to the %TEMP%
    folder.
    FIle will be deleted in exit ;)

--- Media Converter/src/com/github/cshubhamrao/MediaConverter/Library/FFMpegLoader.java
+++ Media Converter/src/com/github/cshubhamrao/MediaConverter/Library/FFMpegLoader.java
@@ -16,10 +16,48 @@
  */
 package com.github.cshubhamrao.MediaConverter.Library;
 
-/** This class extracts and loads the ffmpeg executable from the jar
+import com.github.cshubhamrao.MediaConverter.MainUI;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class extracts the ffmpeg executable from .jar file and stores it in
+ * %temp%
  *
  * @author Shubham
  */
-public class FFMpegLoader {
-    
+public class FFMpegLoader implements Runnable {
+
+    File ffmpegExecutable;
+    private final String ffmpegRoot = "com/github/cshubhamrao/MediaConverter/FFMpeg/";
+
+    @Override
+    public void run() {
+        try {
+            ffmpegExecutable = File.createTempFile("MediaConverter", ".exe");
+            ffmpegExecutable.deleteOnExit();
+        } catch (IOException ex) {
+            Logger.getLogger(FFMpegLoader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try (
+                OutputStream outputStream = new FileOutputStream(ffmpegExecutable);
+                InputStream inputStream = MainUI.class.getClassLoader()
+                        .getResourceAsStream(ffmpegRoot + "ffmpeg")) {
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+
+    }
 }

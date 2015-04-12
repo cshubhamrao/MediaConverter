5c0a14481670ce5f359ec854a70317c27264c681
Shubham Rao <cshubhamrao@gmail.com>
2015-04-12 16:28:56 +0530

    Its 2015 already

--- src/main/java/com/github/cshubhamrao/MediaConverter/Library/FFMpegLoader.java
+++ src/main/java/com/github/cshubhamrao/MediaConverter/Library/FFMpegLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Shubham
+ * Copyright (C) 2015 Shubham
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
